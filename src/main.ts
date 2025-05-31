bf66241344836340436542bb8d29b624ec52a9e6
beyondworks <beyondworks.br@gmail.com>
2025-06-01 01:20:23 +0900

    first commit

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // CORS 설정
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('비욘드 오더 허브 API')
+    .setDescription('User, Product, Order 등 주요 엔드포인트 명세')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
+  await app.listen(4000);
+}
+bootstrap();
